102bdc188703d3459b4018459925d164a34c78d6
aa01088920723-beep <aa01088920723@gmail.com>
2026-09-18 16:51:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // 구글 Apps Script 웹 앱 URL 적용 완료
-const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbx12xXDaTkocThQinpDswVrc6Z3UfIkLDVIJSjKs-VgheRpHp6bJSU4E_yyrfijz30K/exec';
+const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbzNdp-CBLeZlLiO02frBXZE-VlPe8K4Q64gATJsTa_EuCPzkt1IFabrq3GfvYX5Hjcw/exec';
 
 app.post('/', async (req, res) => {
   const userMessage = req.body.userRequest ? req.body.userRequest.utterance : "";
